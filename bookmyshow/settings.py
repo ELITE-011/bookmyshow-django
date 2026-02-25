8dd4a230bfc4b57eaae891447e4509ab5cf8567d
ELITE-011 <159515043+ELITE-011@users.noreply.github.com>
2026-02-25 16:02:10 +0530

    Cleaned requirements for Render

--- bookmyshow/settings.py
+++ bookmyshow/settings.py
@@ -70,12 +70,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'bookmyshow.wsgi.application'
 
 
-# DATABASE
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get(
+            "DATABASE_URL",
+            f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+        )
+    )
 }
 
 
