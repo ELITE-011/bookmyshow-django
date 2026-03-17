650dc781855ded25da60da1695f852d595033cbd
ELITE-011 <159515043+ELITE-011@users.noreply.github.com>
2026-03-17 14:32:27 +0530

    Fix email backend for Render

--- bookmyshow/settings.py
+++ bookmyshow/settings.py
@@ -129,4 +129,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
