e5226057f1bd178c5214c36880eafc7ef17ee449
ELITE-011 <159515043+ELITE-011@users.noreply.github.com>
2026-05-24 11:54:48 +0530

    switched to sqlite

--- bookmyshow/settings.py
+++ bookmyshow/settings.py
@@ -76,14 +76,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'bookmyshow.wsgi.application'
 
 
-# DATABASE
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get(
-            "DATABASE_URL",
-            f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-        )
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
