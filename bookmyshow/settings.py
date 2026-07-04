f876456a92ab008213a93f43151507b9911cdd44
ELITE-011 <159515043+ELITE-011@users.noreply.github.com>
2026-07-04 20:17:29 +0530

    Configure PostgreSQL for Render

--- bookmyshow/settings.py
+++ bookmyshow/settings.py
@@ -4,6 +4,7 @@ Django settings for bookmyshow project.
 
 from pathlib import Path
 import os
+import dj_database_url
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -74,10 +75,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'bookmyshow.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 # PASSWORD VALIDATION
