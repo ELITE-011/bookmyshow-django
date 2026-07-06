f59e7f93897c1fd71b16c99f68f5fe6bdff3438b
ELITE-011 <159515043+ELITE-011@users.noreply.github.com>
2026-07-06 20:02:04 +0530

    Debug email sending

--- bookmyshow/settings.py
+++ bookmyshow/settings.py
@@ -125,7 +125,7 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
+EMAIL_TIMEOUT = 10
 import os
 from dotenv import load_dotenv
 
